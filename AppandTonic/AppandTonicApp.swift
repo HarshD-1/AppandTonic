c1e851817ef291c90942f487627acd526d5568da
Harsh D <hdivecha24@fed.idserve.net>
2024-11-04 10:34:13 +0100

    Initial Commit

---
+++ AppandTonic/AppandTonicApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppandTonicApp.swift
+//  AppandTonic
+//
+//  Created by Harsh Divecha on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct AppandTonicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
