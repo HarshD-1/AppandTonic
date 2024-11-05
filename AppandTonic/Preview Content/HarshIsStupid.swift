0d613e570ef5a084d5f8b499a692f1af5bb93c6b
Luigi Donnino <luigidonnino@gmail.com>
2024-11-05 11:59:18 +0100

    commit 1

---
+++ AppandTonic/Preview Content/HarshIsStupid.swift
@@ -0,0 +1,18 @@
+//
+//  HarshIsStupid.swift
+//  AppandTonic
+//
+//  Created by Luigi Donnino on 05/11/24.
+//
+
+import SwiftUI
+
+struct HarshIsStupid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HarshIsStupid()
+}
